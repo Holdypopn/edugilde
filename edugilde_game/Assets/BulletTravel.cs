009415d7d11f7204901dd258dfde59bdf09d3a87
AsApp9 <peter.rehders@gmx.de>
2023-02-19 19:45:29 +0100

    bullet

---
+++ edugilde_game/Assets/BulletTravel.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletTravel : MonoBehaviour
+{   
+    public float speed = 2;
+    internal void destroySelf()
+    {
+        gameObject.SetActive(false);
+        Destroy(gameObject);
+    }
+
+    private void Awake() 
+    {
+        Invoke("destroySelf", 2);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(speed * Time.deltaTime * Vector2.up);
+    }
+}
