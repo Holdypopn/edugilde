452b06fd12e75758249fb86298b0f95bc622aa9f
Holdypopn <dominik.n8@googlemail.com>
2023-02-21 20:56:38 +0100

    Added Player movement restrictions

--- edugilde_game/Assets/playerHandling2.cs
+++ edugilde_game/Assets/playerHandling2.cs
@@ -6,21 +6,40 @@ public class playerHandling2 : MonoBehaviour
 {
     public float speed = 12;
     private Rigidbody2D Player;
-    public Camera cam;
+    private Vector3 stageDimensions;
+    private float xBorder;
+    private float yBorder;
+
     
     // Start is called before the first frame update
     void Start()
     {
-         Player = GetComponent<Rigidbody2D>();
-         Vector3 minScreenBounce = cam.ScreenToWorldPoint(new Vector3(0, 0, 0));
-         Vector3 maxScreenBounce = cam.ScreenToWorldPoint(new Vector3(11, 11, 0));
+        Player = GetComponent<Rigidbody2D>();
+
+        stageDimensions = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+        // Could change the "1" to half player size
+        xBorder = stageDimensions.x - 1;
+        yBorder = stageDimensions.y - 1;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Player Input Movement
         var horizontalInput = Input.GetAxis("Horizontal") * speed;
         var verticalInput = Input.GetAxis("Vertical") * speed;
         Player.velocity = new Vector2(horizontalInput, verticalInput);
+
+        // Player movement restrictions (borders)
+        if(transform.position.x > xBorder)
+            transform.position = new Vector3(xBorder, transform.position.y, 0);
+        else if(transform.position.x < -xBorder)
+            transform.position = new Vector3(-xBorder, transform.position.y, 0);
+
+        if(transform.position.y > yBorder)
+            transform.position = new Vector3(transform.position.x, yBorder, 0);
+        else if(transform.position.y < -yBorder)
+            transform.position = new Vector3(transform.position.x, -yBorder, 0);
+
     }
 }
