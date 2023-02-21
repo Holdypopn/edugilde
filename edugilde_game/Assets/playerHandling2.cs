f3636f652e08aad63a96127116064aa779b0088a
AsApp9 <peter.rehders@gmx.de>
2023-02-21 20:20:12 +0100

    cam

--- edugilde_game/Assets/playerHandling2.cs
+++ edugilde_game/Assets/playerHandling2.cs
@@ -6,11 +6,14 @@ public class playerHandling2 : MonoBehaviour
 {
     public float speed = 12;
     private Rigidbody2D Player;
+    public Camera cam;
     
     // Start is called before the first frame update
     void Start()
     {
          Player = GetComponent<Rigidbody2D>();
+         Vector3 minScreenBounce = cam.ScreenToWorldPoint(new Vector3(0, 0, 0));
+         Vector3 maxScreenBounce = cam.ScreenToWorldPoint(new Vector3(11, 11, 0));
     }
 
     // Update is called once per frame
